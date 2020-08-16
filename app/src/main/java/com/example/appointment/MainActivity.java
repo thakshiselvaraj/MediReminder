95517e66ab795470d47367dd972dca624fa15342
shey9855 <shehaniwije1998@gmail.com>
2020-08-17 05:29:36 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/appointment/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.appointment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button5;
+    private Button button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button5 = (Button) findViewById(R.id.button5);
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opennextpage();
+            }
+        });
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openaddpage();
+            }
+        });
+    }
+
+    public void opennextpage(){
+        Intent intent = new Intent(this, NewAppointment.class);
+        startActivity(intent);
+    }
+
+    public void openaddpage(){
+        Intent intent = new Intent(this, careCenter.class);
+        startActivity(intent);
+    }
+}
